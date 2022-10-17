90ffe3c50e0ec79cef14fa02e52097f0737f911b
Christian Johnson <cartian4@gmail.com>
2022-10-17 06:29:35 -0400

    Add functionality to create a grid

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+grid();
+
+// Create a 9x9 grid
+function grid() {
+    createRow(9);
+    createColumn(9);
+}
+
+function createRow(rowNum) {
+    for (let i = 0; i < rowNum; i++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    }
+}
+
+function createColumn(cellNum) {
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = 0; j <cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        }
+    }
+}
